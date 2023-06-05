3c3384f7f77afecc25147f59f56ebdc6d2d24bd5
s0m31 <admin@nwolfhub.org>
2023-06-05 10:55:51 +0300

    Continue creating project page, remove auto generated pages from frontend dir and add them to gitignore

--- src/main/java/org/nwolfhub/projects/Project.java
+++ src/main/java/org/nwolfhub/projects/Project.java
@@ -3,10 +3,12 @@ package org.nwolfhub.projects;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.progressbar.ProgressBar;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasDynamicTitle;
@@ -52,9 +54,45 @@ public class Project extends AppLayout implements HasDynamicTitle, HasUrlParamet
             setContent(loadingDiv);
             ObjectDao dao = new ObjectDao(ProjectsApplication.context.getBean("hibernateController", HibernateController.class));
             org.nwolfhub.projects.database.model.Project project = dao.getProject(id);
+            Div mainDiv = new Div();
+            VerticalLayout centeredBox = new VerticalLayout();
+            Label projectName = new Label(project.getName());
+            centeredBox.add(projectName);
+            centeredBox.setAlignItems(FlexComponent.Alignment.CENTER);
+            centeredBox.add(new HorizontalDivider("black"));
+            mainDiv.add(centeredBox);
+            HorizontalLayout layout = new HorizontalLayout();
+            VerticalDivider divider = new VerticalDivider();
+            Label descriptionLabel = new Label(project.getDescription());
+            descriptionLabel.getStyle().set("max-width", "70%").set("word-wrap", "break-word");
+            layout.add(descriptionLabel);
+            divider.getStyle().set("margin-left", "auto").set("margin-right", "0");
+            layout.add(divider);
+            mainDiv.add(layout);
+            setContent(mainDiv);
         } catch (NumberFormatException e) {
             e.printStackTrace();
             UI.getCurrent().access(() -> UI.getCurrent().getPage().setLocation("/"));
+        }
+    }
+    public static class HorizontalDivider extends Span {
+        public HorizontalDivider() {
+            getStyle().set("background-color", "white");
+            getStyle().set("flex", "0 0 1px");
+            getStyle().set("margin-top", "0px");
+            getStyle().set("align-self", "stretch");
+        }
+        public HorizontalDivider(String color) {
+            getStyle().set("background-color", color);
+            getStyle().set("flex", "0 0 1px");
+            getStyle().set("margin-top", "0px");
+            getStyle().set("align-self", "stretch");
+        }
+    }
+    public static class VerticalDivider extends Div {
+        public VerticalDivider() {
+            getStyle().set("border-left", "1px solid #000");
+            getStyle().set("height", "500px");
         }
     }
 }
